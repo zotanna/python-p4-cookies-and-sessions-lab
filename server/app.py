3d1c0224764d9126c5885cb34082a0cddb97a3d7
denis <denniiskim@gmail.com>
2024-03-20 03:58:17 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_compact = False
 
 migrate = Migrate(app, db)
 
@@ -18,17 +18,21 @@ db.init_app(app)
 @app.route('/clear')
 def clear_session():
     session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+    return {'message': 'Successfully cleared session data.'}, 200
 
 @app.route('/articles')
 def index_articles():
+    articles = [article.to_dict() for article in Article.query.all()]
+    return make_response(jsonify(articles), 200)
 
-    pass
-
-@app.route('/articles/<int:id>')
+@app.route('/articles/<int:id>', methods=['GET'])
 def show_article(id):
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
 
-    pass
+    if session['page_views'] <= 3:
+        return Article.query.filter(Article.id == id).first().to_dict(), 200
+    return {"message": "Maximum pageview limit reached"}, 401
 
 if __name__ == '__main__':
     app.run(port=5555)
